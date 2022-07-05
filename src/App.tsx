cd6876e8f46654fb16ef5f45caf7ab768514add8
ViaChessLove <85558513+ViaChessLove@users.noreply.github.com>
2022-07-05 08:46:11 +0500

    footer initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { GlobalStyle } from './globalStyles';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <>
       <GlobalStyle/>
       <Navbar />
-      
+      <Footer/>
     </>
   );
 }
