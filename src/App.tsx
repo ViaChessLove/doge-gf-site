c7504d17fa34b3ca62ce927b0bf9a2c0123c765d
ViaChessLove <85558513+ViaChessLove@users.noreply.github.com>
2022-07-05 16:56:55 +0500

    faqqqqqqqqqqqqq

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { GlobalStyle } from './globalStyles';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import Start from './components/Start/Start';
+import Faq from './components/Faq/Faq';
 
 const App = () => {
   return (
     <>
       <GlobalStyle/>
       <Navbar />
+      <Start/>
+      <Faq/>
       <Footer/>
     </>
   );
